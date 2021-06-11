56970439625d82813e53be1676da8350652fefb6
carlos <cdcaripa@hotmail.com>
2021-06-11 18:26:27 -0400

    v5

--- index.php
+++ index.php
@@ -9,10 +9,19 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 $request = Request::createFromGlobals();
 
-$msj = $request->get("name");
+$nombre = $request->get("nombre");
+$edad = $request->get("edad");
+
+$juegos = array(
+    "1991" => "Wolfenstein 3D",
+    "1993" => "DOOM",
+    "1996" => "Quake"
+);
 
 $response = new JsonResponse([
-    "mensaje" => $msj
+    "nombre" => $nombre,
+    "edad" => $edad,
+    "juegos" => $juegos
 ]);
 
 $response->send();
